08e302cb885b33e9f551486b65c109fefdfba83b
Jeffrey Pepin <peffjepin@gmail.com>
2021-09-12 01:14:27 -0400

    add process sharable numpy array interface

---
+++ src/gamelib/sharedmem.py
@@ -0,0 +1,21 @@
+from multiprocessing import shared_memory as sm
+from typing import Tuple, List
+
+import numpy as np
+
+
+class SharedArrays:
+    ArrayStructure = Tuple[str, tuple, np.dtype]
+
+    def __init__(self, blocks: List[ArrayStructure], create: bool = False):
+        self._mem_lookup, self._arr_lookup = dict(), dict()
+        for id_, shape, dtype in blocks:
+            empty_array = np.empty(shape, dtype)
+            shared_memory = sm.SharedMemory(id_, create, empty_array.nbytes)
+            shared_array = np.ndarray(shape, dtype, shared_memory.buf)
+
+            self._mem_lookup[id_] = shared_memory
+            self._arr_lookup[id_] = shared_array
+
+    def __getitem__(self, id_):
+        return self._arr_lookup[id_]
---
+++ tests/slow_tests/test_shared_arrays.py
@@ -0,0 +1,28 @@
+import multiprocessing as mp
+
+import numpy as np
+import pytest
+
+from src.gamelib.sharedmem import SharedArrays
+
+
+class TestSharedArrays:
+    def test_a_write_in_a_second_process_is_seen_in_the_first_process(self):
+        array_spec = ('test', (10, 10), np.uint8)
+        shared_arrays = SharedArrays([array_spec], create=True)
+        shared_arrays['test'][:,:] = 0
+
+        process = mp.Process(target=self.run_in_process, args=(array_spec,))
+        process.start()
+        process.join()
+
+        assert np.all(shared_arrays['test'] == 100)
+
+    @classmethod
+    def run_in_process(cls, array_spec):
+        shared_arrays = SharedArrays([array_spec])
+        shared_arrays['test'][:, :] = 100
+
+
+if __name__ == '__main__':
+    pytest.main()
