aa90c4d0b6390c602e789f27cc03d6318c254318
Jeffrey Pepin <peffjepin@gmail.com>
2021-09-17 22:49:05 -0400

    Refactor pipe reader

--- tests/conftest.py
+++ tests/conftest.py
@@ -51,22 +51,9 @@ def image_file_maker(tmpdir) -> Callable[[Tuple[int, int]], pathlib.Path]:
 
 @pytest.fixture
 def pipe_reader():
-    def _reader(conn: Connection, timeout=1000):
-        """
-        Tries to return value read from pipe within timeout ms. Returns value read or None.
-
-        Default timeout potentially a little high for quick testing, but in practice it should
-        return after only a few cycles on a passing test, and really only aught to take the full
-        1s on failures. This should ensure that test results aren't flaky due to not having enough time
-        to read the pipe.
-        """
-        value = None
-        for _ in range(timeout):
-            if not conn.poll(0):
-                time.sleep(1 / 1_000)
-                continue
-            value = conn.recv()
-            break
-        return value
-
+    def _reader(conn: Connection, timeout=1_000):
+        if not conn.poll(timeout/1_000):
+            return None
+        else:
+            return conn.recv()
     return _reader
