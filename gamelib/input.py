ecb14dfea79568ba0bb198578e78ec47e3d54726
peffjepin <peffjepin@gmail.com>
2021-12-05 01:22:24 -0500

    Beginning to refactor the user input system out of the events module.

---
+++ gamelib/input.py
@@ -0,0 +1,248 @@
+from enum import Enum
+from typing import Any
+
+from gamelib import Event
+
+
+class StringMappingEnum(Enum):
+    @classmethod
+    def map_string(cls, string):
+        for member in cls:
+            if string.lower() in member.value:
+                return member
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return other.lower() in self.value
+        elif isinstance(other, type(self)):
+            return other.name == self.name
+        else:
+            return False
+
+    def __hash__(self):
+        return hash(self.name)
+
+
+class InputType(StringMappingEnum):
+    """Defines which string values map to which keyboard inputs."""
+
+    ESCAPE = ("escape", "esc")
+    SPACE = ("space",)
+    ENTER = ("enter", "return", "\n")
+    PAGE_UP = ("page_up",)
+    PAGE_DOWN = ("page_down",)
+    LEFT = ("left",)
+    RIGHT = ("right",)
+    UP = ("up",)
+    DOWN = ("down",)
+
+    TAB = ("\t", "tab")
+    COMMA = (",", "comma")
+    MINUS = ("-", "minus")
+    PERIOD = (".", "period")
+    SLASH = ("/", "slash")
+    SEMICOLON = (";", "semicolon")
+    EQUAL = ("=", "equal")
+    LEFT_BRACKET = ("[", "left_bracket")
+    RIGHT_BRACKET = ("]", "right_bracket")
+    BACKSLASH = ("\\", "backslash")
+    BACKSPACE = ("back", "backspace")
+    INSERT = ("ins", "insert")
+    DELETE = ("del", "delete")
+    HOME = ("home",)
+    END = ("end",)
+    CAPS_LOCK = ("caps", "caps_lock")
+
+    F1 = ("f1",)
+    F2 = ("f2",)
+    F3 = ("f3",)
+    F4 = ("f4",)
+    F5 = ("f5",)
+    F6 = ("f6",)
+    F7 = ("f7",)
+    F8 = ("f8",)
+    F9 = ("f9",)
+    F10 = ("f10",)
+    F11 = ("f11",)
+    F12 = ("f12",)
+
+    NUMBER_0 = ("0",)
+    NUMBER_1 = ("1",)
+    NUMBER_2 = ("2",)
+    NUMBER_3 = ("3",)
+    NUMBER_4 = ("4",)
+    NUMBER_5 = ("5",)
+    NUMBER_6 = ("6",)
+    NUMBER_7 = ("7",)
+    NUMBER_8 = ("8",)
+    NUMBER_9 = ("9",)
+
+    NUMPAD_0 = ("n_0", "numpad_0")
+    NUMPAD_1 = ("n_1", "numpad_1")
+    NUMPAD_2 = ("n_2", "numpad_2")
+    NUMPAD_3 = ("n_3", "numpad_3")
+    NUMPAD_4 = ("n_4", "numpad_4")
+    NUMPAD_5 = ("n_5", "numpad_5")
+    NUMPAD_6 = ("n_6", "numpad_6")
+    NUMPAD_7 = ("n_7", "numpad_7")
+    NUMPAD_8 = ("n_8", "numpad_8")
+    NUMPAD_9 = ("n_9", "numpad_9")
+
+    A = ("a", "key_a")
+    B = ("b", "key_b")
+    C = ("c", "key_c")
+    D = ("d", "key_d")
+    E = ("e", "key_e")
+    F = ("f", "key_f")
+    G = ("g", "key_g")
+    H = ("h", "key_h")
+    I = ("i", "key_i")
+    J = ("j", "key_j")
+    K = ("k", "key_k")
+    L = ("l", "key_l")
+    M = ("m", "key_m")
+    N = ("n", "key_n")
+    O = ("o", "key_o")
+    P = ("p", "key_p")
+    Q = ("q", "key_q")
+    R = ("r", "key_r")
+    S = ("s", "key_s")
+    T = ("t", "key_t")
+    U = ("u", "key_u")
+    V = ("v", "key_v")
+    W = ("w", "key_w")
+    X = ("x", "key_x")
+    Y = ("y", "key_y")
+    Z = ("z", "key_z")
+
+    MOUSE1 = ("mouse1", "mouse_1", "mouse_left")
+    MOUSE2 = ("mouse2", "mouse_2", "mouse_right")
+    MOUSE3 = ("mouse3", "mouse_3", "mouse_middle")
+    SCROLL = ("scroll", "mouse_scroll")
+
+
+class InputMod(StringMappingEnum):
+    SHIFT = ("shift", "mod1")
+    CTRL = ("control", "ctrl", "mod2")
+    ALT = ("alt", "mod3")
+
+
+class InputAction(StringMappingEnum):
+    PRESS = ("press", "down", "on_press", "on_down")
+    RELEASE = ("release", "up", "on_release", "on_up")
+    IS_PRESSED = ("pressed", "ispressed", "isdown", "is_pressed", "is_down")
+
+
+class _InputHandlerTree:
+    def __init__(self):
+        mod_lookup = {
+            # SHIFT, CTRL, ALT
+            (True, True, True): None,
+            (True, True, False): None,
+            (True, False, True): None,
+            (False, True, True): None,
+            (False, False, True): None,
+            (False, True, False): None,
+            (True, False, False): None,
+            (False, False, False): None,
+        }
+        self._registry = {
+            InputAction.PRESS: mod_lookup.copy(),
+            InputAction.RELEASE: mod_lookup.copy(),
+            InputAction.IS_PRESSED: mod_lookup.copy(),
+            None: mod_lookup.copy(),
+        }
+
+    def register(self, callback, modifiers=(), action=None):
+        self._registry[action][self._mods_to_key(modifiers)] = callback
+
+    def get_callback(self, mods=(), action=None) -> Any:
+        mod_lookup = self._registry[action]
+        for perm in self._mod_key_permutations(mods):
+            callback = mod_lookup[perm]
+            if callback is not None:
+                return callback
+        return None
+
+    @staticmethod
+    def _mods_to_key(mods):
+        return (
+            InputMod.SHIFT in mods,
+            InputMod.CTRL in mods,
+            InputMod.ALT in mods,
+        )
+
+    @classmethod
+    def _mod_key_permutations(cls, mods):
+        key = cls._mods_to_key(mods)
+
+        # first look for exact match
+        yield key
+
+        # try flipping one mod off
+        for i in range(3):
+            if key[i]:
+                yield *key[:i], False, *key[i + 1 :]
+
+        # try flipping two mods off
+        if key[0] and key[1]:
+            yield False, False, key[2]
+        if key[0] and key[2]:
+            yield False, key[1], False
+        if key[1] and key[2]:
+            yield key[0], False, False
+
+        # fallback to no mods
+        yield False, False, False
+
+
+class InputSchema:
+    def __init__(self, *designations):
+        self._callback_trees = dict()
+
+        for desc in designations:
+            input_type, *optional, callback = desc
+
+            if isinstance(input_type, str):
+                input_type = InputType.map_string(input_type)
+
+            mods = []
+            action = None
+            for arg in optional:
+                # str might be mod or action
+                if isinstance(arg, str):
+                    if mod := InputMod.map_string(arg):
+                        mods.append(mod)
+                    else:
+                        action = InputAction.map_string(arg)
+
+                # list/tuple are mods
+                elif isinstance(arg, list) or isinstance(arg, tuple):
+                    for mod in arg:
+                        if isinstance(mod, str):
+                            mods.append(InputMod.map_string(mod))
+                        else:
+                            mods.append(mod)
+
+            if input_type not in self._callback_trees:
+                self._callback_trees[input_type] = _InputHandlerTree()
+            callback_tree = self._callback_trees[input_type]
+            callback_tree.register(callback, modifiers=mods, action=action)
+
+    def __call__(self, event):
+        callback_tree = self._callback_trees.get(event.type)
+        if not callback_tree:
+            return
+        callback = callback_tree.get_callback(
+            mods=event.modifiers,
+            action=event.action,
+        )
+        if callback:
+            callback(event)
+
+
+class InputEvent(Event):
+    __slots__ = ["type", "action", "modifiers"]
+
+    def __init__(self, type, action=None, modifiers=None):
+        super().__init__(type, action, modifiers or ())
---
+++ tests/unit_tests/test_input.py
@@ -0,0 +1,130 @@
+import pytest
+
+from gamelib.input import (
+    InputType as Type,
+    InputMod as Mod,
+    InputAction as Action,
+    InputEvent as Event,
+    InputSchema,
+)
+
+from tests.conftest import RecordedCallback
+
+
+@pytest.mark.parametrize(
+    "string, enum, expected",
+    (
+        ("esc", Type, Type.ESCAPE),
+        ("ESC", Type, Type.ESCAPE),
+        ("Esc", Type, Type.ESCAPE),
+        ("Escape", Type, Type.ESCAPE),
+        ("\n", Type, Type.ENTER),
+        (".", Type, Type.PERIOD),
+        ("f1", Type, Type.F1),
+        ("0", Type, Type.NUMBER_0),
+        ("n_0", Type, Type.NUMPAD_0),
+        ("numpad_0", Type, Type.NUMPAD_0),
+        ("A", Type, Type.A),
+        ("a", Type, Type.A),
+        ("key_a", Type, Type.A),
+        ("mouse1", Type, Type.MOUSE1),
+        ("mouse_left", Type, Type.MOUSE1),
+        ("shift", Mod, Mod.SHIFT),
+        ("ctrl", Mod, Mod.CTRL),
+        ("CTRL", Mod, Mod.CTRL),
+        ("control", Mod, Mod.CTRL),
+        ("press", Action, Action.PRESS),
+        ("on_press", Action, Action.PRESS),
+        ("down", Action, Action.PRESS),
+    ),
+)
+def test_mapping_strings_to_enum(string, enum, expected):
+    assert enum.map_string(string) == expected
+    assert expected == string
+
+
+class TestInputSchema:
+    def test_base_case(self):
+        callbacks = [RecordedCallback() for _ in range(3)]
+        schema = InputSchema(
+            ("a", callbacks[0]),
+            ("b", callbacks[1]),
+            ("c", callbacks[2]),
+        )
+
+        event = Event(Type.A)
+        schema(event)
+        assert [cb.called for cb in callbacks] == [1, 0, 0]
+
+    def test_with_input_action(self):
+        callbacks = [RecordedCallback() for _ in range(3)]
+        schema = InputSchema(
+            ("a", callbacks[0]),
+            ("a", "down", callbacks[1]),
+            ("a", "up", callbacks[2]),
+        )
+
+        event = Event(Type.A, Action.PRESS)
+        schema(event)
+        assert [cb.called for cb in callbacks] == [0, 1, 0]
+
+        event = Event(Type.A, Action.RELEASE)
+        schema(event)
+        assert [cb.called for cb in callbacks] == [0, 1, 1]
+
+    def test_with_modifiers(self):
+        callbacks = [RecordedCallback() for _ in range(2)]
+        schema = InputSchema(
+            ("a", "shift", callbacks[0]),
+            ("a", ("shift", "ctrl"), callbacks[1]),
+        )
+
+        modifiers = (Mod.SHIFT,)
+        event = Event(Type.A, modifiers=modifiers)
+        schema(event)
+        assert [cb.called for cb in callbacks] == [1, 0]
+
+        modifiers = (Mod.SHIFT, Mod.CTRL)
+        event = Event(Type.A, modifiers=modifiers)
+        schema(event)
+        assert [cb.called for cb in callbacks] == [1, 1]
+
+        modifiers = (Mod.SHIFT, Mod.CTRL, Mod.ALT)
+        event = Event(Type.A, modifiers=modifiers)
+        schema(event)
+        assert [cb.called for cb in callbacks] == [1, 2]
+
+    @pytest.mark.parametrize(
+        "event, callback_index",
+        (
+            (Event(Type.A, Action.PRESS, (Mod.SHIFT, Mod.CTRL)), 0),
+            (Event(Type.A, Action.PRESS, (Mod.SHIFT, Mod.CTRL, Mod.ALT)), 1),
+            (Event(Type.A, Action.PRESS, (Mod.ALT, Mod.CTRL)), 2),
+            (Event(Type.A), 3),
+            (Event(Type.A, Action.RELEASE, (Mod.ALT,)), 4),
+            (Event(Type.A, Action.RELEASE, (Mod.SHIFT, Mod.CTRL, Mod.ALT)), 5),
+            (Event(Type.A, Action.RELEASE, (Mod.SHIFT, Mod.CTRL)), 6),
+            (Event(Type.B, Action.PRESS, (Mod.CTRL,)), 7),
+            (Event(Type.B, Action.PRESS, (Mod.CTRL, Mod.SHIFT, Mod.ALT)), 8),
+            (Event(Type.B, Action.PRESS, (Mod.ALT, Mod.CTRL)), 9),
+        ),
+    )
+    def test_all_options_integrated(self, event, callback_index):
+        callbacks = [RecordedCallback() for _ in range(10)]
+        schema = InputSchema(
+            ("a", "down", callbacks[0]),
+            ("a", "down", ("shift", "alt"), callbacks[1]),
+            ("a", "down", "alt", callbacks[2]),
+            ("a", callbacks[3]),
+            ("a", "up", callbacks[4]),
+            ("a", "up", ("shift", "ctrl", "alt"), callbacks[5]),
+            ("a", "up", "ctrl", callbacks[6]),
+            ("b", "down", callbacks[7]),
+            ("b", "down", ("shift", "alt"), callbacks[8]),
+            ("b", "down", "alt", callbacks[9]),
+        )
+
+        schema(event)
+        expected = [0] * 10
+        expected[callback_index] = 1
+        assert [cb.called for cb in callbacks] == expected
