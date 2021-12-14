5b21b7c65579eaedd5e01081f14ac225f619fa69
peffjepin <peffjepin@gmail.com>
2021-12-14 17:29:18 -0500

    Black autoformatter.

--- tests/unit_tests/test_geometry.py
+++ tests/unit_tests/test_geometry.py
@@ -21,13 +21,13 @@ def test_grid_mesh():
 @pytest.mark.parametrize(
     "vector, matrix, expected",
     (
-            ((1, 1, 1), Mat3.rotate_about_x(90), (1, -1, 1)),
-            ((1, 1, 1), Mat3.rotate_about_axis((1, 0, 0), 90), (1, -1, 1)),
-            ((1, 1, 1), Mat3.rotate_about_y(90), (1, 1, -1)),
-            ((1, 1, 1), Mat3.rotate_about_axis((0, 1, 0), 90), (1, 1, -1)),
-            ((1, 1, 1), Mat3.rotate_about_z(90), (-1, 1, 1)),
-            ((1, 1, 1), Mat3.rotate_about_axis((0, 0, 1), 90), (-1, 1, 1)),
-            ((1, 1, 1), Mat3.rotate_about_axis((-1, 1, 0), 180), (-1, -1, -1)),
+        ((1, 1, 1), Mat3.rotate_about_x(90), (1, -1, 1)),
+        ((1, 1, 1), Mat3.rotate_about_axis((1, 0, 0), 90), (1, -1, 1)),
+        ((1, 1, 1), Mat3.rotate_about_y(90), (1, 1, -1)),
+        ((1, 1, 1), Mat3.rotate_about_axis((0, 1, 0), 90), (1, 1, -1)),
+        ((1, 1, 1), Mat3.rotate_about_z(90), (-1, 1, 1)),
+        ((1, 1, 1), Mat3.rotate_about_axis((0, 0, 1), 90), (-1, 1, 1)),
+        ((1, 1, 1), Mat3.rotate_about_axis((-1, 1, 0), 180), (-1, -1, -1)),
     ),
 )
 def test_3d_rotations(vector, matrix, expected):
