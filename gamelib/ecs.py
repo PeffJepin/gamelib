2ac3b8536c39ccad5fc573026ab20e13aa432222
peffjepin <peffjepin@gmail.com>
2022-01-16 23:50:11 -0500

    Renamed component.py to ecs.py with the addition of the Entity class which is used to link components together underneath an individual identity.

--- gamelib/ecs.py
+++ gamelib/ecs.py
@@ -18,10 +18,177 @@
 import itertools
 import threading
 
+from typing import Iterable
+
 import numpy as np
 
 
 _STARTING_LENGTH = 10
+
+
+class DynamicArrayManager:
+    def __init__(self, **dtypes):
+        self._internal_length = _STARTING_LENGTH
+        self._arrays = {
+            name: np.empty(self._internal_length, dtype)
+            for name, dtype in dtypes.items()
+        }
+        self._ids = np.empty(self._internal_length, int)
+        self._index_lookup = np.empty(self._internal_length, int)
+        self._ids[:] = -1
+        self._index_lookup[:] = -1
+        self._length = 0
+        self._counter = itertools.count(0)
+        self._recycled_ids = []
+
+    def __getattr__(self, attr):
+        if attr in self._arrays:
+            return self._arrays[attr][: self._length]
+        if attr == "ids":
+            return self._ids[: self._length]
+
+    def __getitem__(self, id):
+        try:
+            index = self._index_lookup[id]
+        except IndexError:
+            return None
+
+        if index == -1:
+            return None
+        return _DynamicArrayEntry(id, self)
+
+    def __delitem__(self, id):
+        index = self._index_lookup[id]
+        if index == -1:
+            return
+
+        final_index = self._length - 1
+        if index != final_index:
+            final_id = self._ids[final_index]
+            # swap good data (end of the array) into the deleted index
+            for array in self._arrays.values():
+                array[index] = array[final_index]
+            # correct the id and index lookup arrays to reflect the swap
+            self._ids[index] = final_id
+            self._index_lookup[final_id] = index
+
+        self._recycled_ids.append(id)
+        self._length -= 1
+        # mask off the the final entry where there is now stale data
+        self._index_lookup[id] = -1
+        self._consider_shrinking()
+
+    def __len__(self):
+        return len(self._ids)
+
+    def __iter__(self):
+        return (getattr(self, field) for field in self._arrays)
+
+    def new_entry(self, *args, **kwargs):
+        if self._recycled_ids:
+            id = self._recycled_ids.pop(0)
+        else:
+            id = next(self._counter)
+
+        if self._length >= self._internal_length:
+            self._reallocate_arrays(self._length * 1.5)
+        if id >= len(self._index_lookup):
+            new_lookup = _reallocate_array(self._index_lookup, id + 1, fill=-1)
+            self._index_lookup = new_lookup
+
+        index = self._length
+        self._length += 1
+        self._ids[index] = id
+        self._index_lookup[id] = index
+        if args:
+            for arr, val in zip(self._arrays.values(), args):
+                arr[index] = val
+        elif kwargs:
+            for name, val in kwargs.items():
+                self._arrays[name][index] = val
+
+        return _DynamicArrayEntry(id, self)
+
+    def get_index(self, id):
+        try:
+            return self._index_lookup[id]
+        except IndexError:
+            return None
+
+    def get_raw_arrays(self):
+        return self._arrays
+
+    def clear(self):
+        self._reallocate_arrays(_STARTING_LENGTH)
+        self._index_lookup = np.empty(_STARTING_LENGTH, int)
+        self._index_lookup[:] = -1
+        self._internal_length = _STARTING_LENGTH
+        self._length = 0
+        self._counter = itertools.count(0)
+        self._recycled_ids = []
+
+    def _reallocate_arrays(self, new_length):
+        new_length = max(int(new_length), _STARTING_LENGTH)
+        for name, array in self._arrays.items():
+            self._arrays[name] = _reallocate_array(array, new_length, fill=-1)
+        self._ids = _reallocate_array(self._ids, new_length, fill=-1)
+        self._internal_length = new_length
+
+    def _consider_shrinking(self):
+        # consider shrinking id lookup
+        if len(self._index_lookup) >= self._length * 1.8 and self._length > 0:
+            largest_id = np.argwhere(self._index_lookup != -1)[-1]
+            if largest_id <= self._length * 1.4:
+                self._index_lookup = _reallocate_array(
+                    self._index_lookup, largest_id + 1, -1
+                )
+                self._counter = itertools.count(largest_id + 1)
+                self._recycled_ids = list(
+                    filter(lambda id: id < largest_id, self._recycled_ids)
+                )
+
+        # consider shrinking component data arrays
+        if self._length <= self._internal_length * 0.65:
+            self._reallocate_arrays(self._length)
+
+
+class _DynamicArrayEntry:
+    _initialized = False
+
+    def __init__(self, id, manager):
+        self._manager = manager
+        self._id = id
+        self._initialized = True
+
+    @property
+    def id(self):
+        return self._id
+
+    def __getattr__(self, attr):
+        arrays = self._manager.get_raw_arrays()
+        index = self._manager.get_index(self._id)
+        if attr not in arrays:
+            raise AttributeError(f"{attr} not in {arrays.keys()!r}")
+        return arrays[attr][index]
+
+    def __setattr__(self, attr, value):
+        if not self._initialized:
+            super().__setattr__(attr, value)
+        else:
+            arrays = self._manager.get_raw_arrays()
+            index = self._manager.get_index(self._id)
+            if attr not in arrays:
+                raise AttributeError(f"{attr} not in {arrays.keys()!r}")
+            arrays[attr][index] = value
+
+    def __iter__(self):
+        index = self._manager.get_index(self._id)
+        return iter(a[index] for a in self._manager.get_raw_arrays().values())
+
+    def __eq__(self, other):
+        if not isinstance(other, Iterable):
+            return False
+        return all(v1 == v2 for v1, v2 in zip(self, other))
 
 
 class _ComponentType(type):
@@ -264,7 +431,7 @@ class Component(metaclass=_ComponentType):
             id of the component to be deleted.
         """
 
-        id = target if isinstance(target, int) else target.id
+        id = target.id if isinstance(target, cls) else target
         index = cls.id_lookup[id]
 
         # component doesn't actually exist
@@ -313,6 +480,10 @@ class Component(metaclass=_ComponentType):
         cls._max_length = new_length
 
     @classmethod
+    def indices_from_ids(cls, ids):
+        return cls.id_lookup[ids]
+
+    @classmethod
     def _get_new_id(cls):
         """Requests an id to assign to a newly created component."""
 
@@ -346,6 +517,113 @@ class Component(metaclass=_ComponentType):
         # consider shrinking component data arrays
         if cls.length <= cls._max_length * 0.65:
             cls.reallocate(cls.length)
+
+
+class _EntityMeta(type):
+    def __getattr__(cls, name):
+        return _EntityMask(cls, cls._fields[name])
+
+    def get_component_ids(cls, component_type):
+        attribute_name = ""
+        for name, type in cls._fields.items():
+            if type is component_type:
+                attribute_name = name
+                break
+        if not attribute_name:
+            raise ValueError(f"Couldn't find a field for {component_type!r}.")
+        return getattr(cls._arrays, attribute_name)
+
+
+class _EntityMask:
+    def __init__(self, entity_type, component_type):
+        self._entity = entity_type
+        self._component = component_type
+
+    def __getattr__(self, name):
+        ids = self._entity.get_component_ids(self._component)
+        indices = self._component.indices_from_ids(ids)
+        return getattr(self._component, name)[indices]
+
+
+class Entity(metaclass=_EntityMeta):
+    def __init_subclass__(cls, **kwargs):
+        cls._fields = cls.__dict__.get("__annotations__", {})
+        if not cls._fields:
+            raise AttributeError("No attributes have been annotated.")
+        id_fields = dict()
+        for name, dtype in cls._fields.items():
+            if not issubclass(dtype, Component):
+                raise AttributeError(
+                    "Expected {dtype!r} to be a Component subclass."
+                )
+            id_fields[name] = int
+        cls._arrays = DynamicArrayManager(**id_fields)
+
+    def __new__(cls, *args, _id=None, **kwargs):
+        if _id is None:
+            if args:
+                component_ids = tuple(arg.id for arg in args)
+                array_entry = cls._arrays.new_entry(*component_ids)
+            elif kwargs:
+                component_ids = {
+                    name: comp.id for name, comp in kwargs.items()
+                }
+                array_entry = cls._arrays.new_entry(**component_ids)
+            else:
+                raise ValueError("Either *args or **kwargs must be given.")
+        else:
+            array_entry = cls._arrays[_id]
+
+        if array_entry is None:
+            return None
+
+        instance = super().__new__(cls)
+        instance._array_entry = array_entry
+        instance._id = array_entry.id
+        return instance
+
+    def __getattr__(self, name):
+        if self._arrays[self._array_entry.id] is None:
+            return None
+        if name in self._fields:
+            comp_type = self._fields[name]
+            return comp_type.get(getattr(self._array_entry, name))
+
+        raise AttributeError()
+
+    def __eq__(self, other):
+        if not isinstance(other, type(self)):
+            return False
+        else:
+            return all(comp1 == comp2 for comp1, comp2 in zip(self, other))
+
+    def __iter__(self):
+        return iter(getattr(self, name) for name in self._fields)
+
+    @property
+    def id(self):
+        return self._id
+
+    @classmethod
+    def get(cls, id):
+        return cls(_id=id)
+
+    @classmethod
+    def destroy(cls, target):
+        if isinstance(target, cls):
+            id = target.id
+            instance = target
+        else:
+            id = target
+            instance = cls.get(id)
+        for comp in instance:
+            comp.destroy(comp.id)
+        del cls._arrays[id]
+
+    @classmethod
+    def clear(cls):
+        for id in cls._arrays.ids:
+            cls.destroy(id)
 
 
 def _reallocate_array(array, new_length, fill=0):
---
+++ tests/unit/test_ecs.py
@@ -0,0 +1,422 @@
+import numpy as np
+
+import pytest
+import threading
+import time
+
+from gamelib import ecs
+
+
+class TestDynamicArrayManager:
+    def test_init(self):
+        arrays = ecs.DynamicArrayManager(field1=float, field2=int)
+
+        assert arrays.field1.dtype == float
+        assert arrays.field2.dtype == int
+        assert all(len(a) == 0 for a in arrays)
+
+    def test_adding_entries(self):
+        arrays = ecs.DynamicArrayManager(field1=float, field2=int)
+
+        entry = arrays.new_entry(1.0, 2)
+        entry2 = arrays.new_entry(2.0, 3)
+
+        assert arrays[entry.id] == (1.0, 2)
+        assert len(arrays.field1) == 2
+        assert len(arrays.field2) == 2
+
+    def test_new_entry_with_kwargs(self):
+        arrays = ecs.DynamicArrayManager(field1=float, field2=int)
+
+        entry = arrays.new_entry(field1=123, field2=321)
+
+        assert arrays[entry.id] == (123.0, 321)
+
+    def test_new_entry_without_values(self):
+        arrays = ecs.DynamicArrayManager(field1=float, field2=int)
+
+        entry = arrays.new_entry()
+
+        assert arrays[entry.id] == entry
+
+    def test_length_of_object_is_internal_length(self):
+        arrays = ecs.DynamicArrayManager(field1=float, field2=int)
+
+        assert len(arrays) > 0
+
+    def test_length_of_fields_are_masked(self):
+        arrays = ecs.DynamicArrayManager(field1=float, field2=int)
+
+        assert len(arrays) > 0
+        assert len(arrays.field1) == 0
+        assert len(arrays.field2) == 0
+
+    def test_adding_entries_forcing_growth(self):
+        arrays = ecs.DynamicArrayManager(field1=float, field2=int)
+        starting_length = len(arrays)
+
+        for i in range(starting_length + 10):
+            arrays.new_entry(i, i)
+
+        assert len(arrays) > starting_length + 10
+
+    def test_out_of_bounds_returns_none(self):
+        arrays = ecs.DynamicArrayManager(field1=float, field2=int)
+        assert arrays[1_000_000] is None
+
+    def test_removing_entries(self):
+        arrays = ecs.DynamicArrayManager(field1=float, field2=int)
+
+        id = arrays.new_entry(1.0, 1).id
+        assert arrays[id] is not None
+        del arrays[id]
+
+        assert arrays[id] is None
+
+    def test_removing_entries_forcing_shrinking(self):
+        arrays = ecs.DynamicArrayManager(field1=float, field2=int)
+
+        starting_length = len(arrays)
+        i = 0
+        entries = []
+        while len(arrays) <= starting_length:
+            entries.append(arrays.new_entry(i, i))
+            i += 1
+            if i >= 50:
+                assert False
+
+        grown_length = len(arrays)
+        for entry in entries:
+            del arrays[entry.id]
+
+        assert len(arrays) < grown_length
+
+    def test_ids_are_recycled(self):
+        arrays = ecs.DynamicArrayManager(field1=float, field2=int)
+
+        entry1 = arrays.new_entry()
+        entry2 = arrays.new_entry()
+        entry3 = arrays.new_entry()
+
+        id1 = entry1.id
+        del arrays[id1]
+        entry4 = arrays.new_entry()
+        entry5 = arrays.new_entry()
+
+        assert entry4.id == id1
+        assert entry5.id == entry3.id + 1
+
+    def test_clear(self):
+        arrays = ecs.DynamicArrayManager(field1=float, field2=int)
+
+        entry1 = arrays.new_entry()
+        entry2 = arrays.new_entry()
+        entry3 = arrays.new_entry()
+        arrays.clear()
+
+        assert len(arrays.field1) == 0
+        assert arrays.new_entry().id == 0
+
+    def test_initial_index_preserves_identity_through_reallocation(self):
+        arrays = ecs.DynamicArrayManager(field1=float, field2=int)
+
+        entries = [arrays.new_entry(i, i) for i in range(50)]
+        for entry in entries[:-1]:
+            # print(f"{len(arrays)=}, {entry.id=}, {len(arrays.field1)=}")
+            print(arrays.field1)
+            print(arrays.get_index(49))
+            del arrays[entry.id]
+
+        # despite the array no longer being large enough for 50 entries,
+        # the 50th index (49) hasn't been deleted so should still work.
+        assert len(arrays) < 50
+        assert arrays[entries[-1].id] == (49, 49)
+
+    def test_field_array(self):
+        arrays = ecs.DynamicArrayManager(field1=float, field2=int)
+
+        entries = [arrays.new_entry(i, i) for i in range(3)]
+        del arrays[entries[1].id]
+
+        # note that the indices for the fields wont necessarily be
+        # the same as the initial indices used. Only access through
+        # the arrays object can preserve identity
+        assert np.all(arrays.field1 == np.array([0, 2], float))
+        assert np.all(arrays.field2 == np.array([0, 2], int))
+
+    def test_mutating_the_internal_data(self):
+        arrays = ecs.DynamicArrayManager(field1=float, field2=int)
+        entry = arrays.new_entry(123.0, 124)
+        assert entry.field1 == 123.0
+        assert entry.field2 == 124
+
+        entry.field1 = 321
+        entry.field2 = 456
+
+        assert arrays[entry.id] == (321, 456)
+
+
+class ExampleComponent(ecs.Component):
+    x: float
+    y: float
+
+
+class ExampleComponent2(ecs.Component):
+    z: float
+    w: float
+
+
+class TestComponent:
+    @pytest.fixture(autouse=True)
+    def cleanup(self):
+        ExampleComponent.reset()
+
+    def test_creating_a_component(self):
+        component = ExampleComponent(123, 124)
+
+        assert component.x == 123
+        assert component.y == 124
+        assert 123 in ExampleComponent.x
+        assert 124 in ExampleComponent.y
+        actual = ExampleComponent.get_raw_arrays()[component.id]
+        assert actual["x"], actual["y"] == (123, 124)
+
+    def test_getting_a_component(self):
+        component = ExampleComponent(1.23, 4.56)
+
+        assert ExampleComponent.get(component.id) == component
+
+    def test_destroying_a_component(self):
+        component1 = ExampleComponent(0.1234, 0.1234)
+        component2 = ExampleComponent(321, 321)
+
+        assert component1 == ExampleComponent.get(component1.id)
+        assert component2 == ExampleComponent.get(component2.id)
+
+        component1.destroy(component1.id)
+        ExampleComponent.destroy(component2.id)
+
+        assert ExampleComponent.get(component1.id) is None
+        assert ExampleComponent.get(component2.id) is None
+
+    def test_length_of_component(self):
+        length = ExampleComponent.length
+        ExampleComponent(0, 0)
+
+        assert ExampleComponent.length == length + 1
+
+    def test_equality_comparison(self):
+        assert ExampleComponent(123.0, 123.0) == (123.0, 123.0)
+        assert ExampleComponent(123.0, 123.0) != (123.1, 123.1)
+
+    def test_getting_an_existing_component_by_id(self):
+        id = ExampleComponent(519, 542).id
+
+        looked_up = ExampleComponent.get(id)
+        assert looked_up == (519, 542)
+
+        looked_up.x = 1024
+        looked_up.y = 1025
+
+        assert ExampleComponent.get(id) == (1024, 1025)
+
+    def test_id_when_created(self):
+        component = ExampleComponent(1234, 1234)
+
+        assert isinstance(component.id, int)
+        assert ExampleComponent.get(component.id) == (1234, 1234)
+        assert ExampleComponent(1234, 1234).id == component.id + 1
+
+    def test_recycling_an_id(self):
+        component1 = ExampleComponent(0, 0)
+        c1_id = component1.id
+        ExampleComponent.destroy(component1.id)
+        component2 = ExampleComponent(1, 1)
+
+        assert component2.id == c1_id
+
+    def test_ids_back_to_0_after_clear(self):
+        for _ in range(10):
+            ExampleComponent(0, 0)
+        ExampleComponent.reset()
+
+        assert ExampleComponent(0, 0).id == 0
+
+    def test_clearing_a_component(self):
+        for _ in range(10):
+            component = ExampleComponent(0, 0)
+
+        assert ExampleComponent.length >= 10
+
+        ExampleComponent.reset()
+
+        assert ExampleComponent.length == 0
+        assert component.x is None
+        assert component.y is None
+
+    def test_masked_after_being_destroyed(self):
+        component1 = ExampleComponent(1, 1)
+        component2 = ExampleComponent(2, 2)
+        component3 = ExampleComponent(3, 3)
+
+        ExampleComponent.destroy(component2.id)
+
+        assert ExampleComponent.get(component1.id) == component1
+        assert ExampleComponent.get(component3.id) == component3
+        assert ExampleComponent.length == 2
+
+    def test_allocating_space_manually(self):
+        max_length = len(ExampleComponent.get_raw_arrays())
+        ExampleComponent.reallocate(max_length + 5)
+        assert len(ExampleComponent.get_raw_arrays()) == max_length + 5
+
+    def test_allocating_space_automatically(self):
+        max_length = len(ExampleComponent.get_raw_arrays())
+        current_length = ExampleComponent.length
+
+        for i in range(max_length - current_length + 1):
+            ExampleComponent(i, i)
+
+        assert len(ExampleComponent.get_raw_arrays()) > max_length
+
+    def test_freeing_space_automatically(self):
+        starting_length = len(ExampleComponent.get_raw_arrays()) * 2
+
+        for i in range(starting_length):
+            ExampleComponent(i, i)
+
+        second_length = len(ExampleComponent.get_raw_arrays())
+        for i in reversed(range(starting_length - 1)):
+            ExampleComponent.destroy(i)
+
+        assert len(ExampleComponent.get_raw_arrays()) < second_length
+
+    def test_mutating_data_by_an_instance(self):
+        component = ExampleComponent(1, 2)
+        assert ExampleComponent.get(component.id) == (1, 2)
+
+        component.x = 132
+        component.y = 1234
+
+        assert (132, 1234) == ExampleComponent.get(component.id)
+
+    def test_mutating_data_by_class_array(self):
+        component1 = ExampleComponent(0, 0)
+        component2 = ExampleComponent(1, 1)
+
+        ExampleComponent.x += 100
+
+        assert (100, 0) == component1
+        assert (101, 1) == component2
+
+    def test_locking_access_using_context_manager(self):
+        instance = ExampleComponent(0, 0)
+        running = True
+
+        def increment(inst):
+            while running:
+                inst.x += 1
+                inst.y += 1
+
+        t = threading.Thread(target=increment, args=(instance,), daemon=True)
+        t.start()
+
+        try:
+            # thread shouldn't be able to increment
+            # a component instance can lock the entire array
+            with instance:
+                first_peek = (instance.x, instance.y)
+                for _ in range(100):
+                    assert first_peek == (instance.x, instance.y)
+
+            # thread should do some increments
+            time.sleep(0.001)
+
+            # thread should be locked out again
+            # the component type can lock the entire array
+            with ExampleComponent:
+                second_peek = (instance.x, instance.y)
+                assert second_peek != first_peek
+                for _ in range(100):
+                    assert second_peek == (instance.x, instance.y)
+        finally:
+            running = False
+
+
+class ExampleEntity(ecs.Entity):
+    comp1: ExampleComponent
+    comp2: ExampleComponent2
+
+
+class TestEntity:
+    @pytest.fixture(autouse=True)
+    def cleanup(self):
+        ExampleEntity.clear()
+
+    def test_binding_components_with_entity(self):
+        entity = ExampleEntity(
+            comp1=ExampleComponent(1, 2), comp2=ExampleComponent2(3, 4)
+        )
+
+        assert entity.comp1.x == 1.0
+        assert entity.comp1.y == 2.0
+        assert entity.comp2.z == 3.0
+        assert entity.comp2.w == 4.0
+
+        c1_id = entity.comp1.id
+        c2_id = entity.comp2.id
+        assert entity.comp1 == ExampleComponent.get(c1_id)
+        assert entity.comp2 == ExampleComponent2.get(c2_id)
+
+    def test_equality_comparison(self):
+        entity1 = ExampleEntity(
+            comp1=ExampleComponent(1, 2), comp2=ExampleComponent2(3, 4)
+        )
+        entity2 = ExampleEntity(
+            comp1=ExampleComponent(1, 2), comp2=ExampleComponent2(3, 4)
+        )
+        entity3 = ExampleEntity(
+            comp1=ExampleComponent(3, 4), comp2=ExampleComponent2(1, 2)
+        )
+
+        assert entity1 == entity2
+        assert entity3 != entity1
+
+    def test_getting_a_previously_created_entity(self):
+        entity = ExampleEntity(
+            comp1=ExampleComponent(1, 2), comp2=ExampleComponent2(3, 4)
+        )
+
+        assert ExampleEntity.get(entity.id) == entity
+
+    def test_destroying_an_entity(self):
+        entity = ExampleEntity(
+            comp1=ExampleComponent(1, 2), comp2=ExampleComponent2(3, 4)
+        )
+        id = entity.id
+        id1 = entity.comp1.id
+        id2 = entity.comp2.id
+
+        ExampleEntity.destroy(entity.id)
+
+        assert ExampleEntity.get(id) is None
+        assert ExampleComponent.get(id1) is None
+        assert ExampleComponent2.get(id2) is None
+        assert entity.comp1 is None
+        assert entity.comp2 is None
+
+    def test_access_to_masked_component_arrays(self):
+        ExampleComponent(123, 123)
+        ExampleComponent(1234, 1234)
+
+        entity1 = ExampleEntity(
+            comp1=ExampleComponent(1, 2), comp2=ExampleComponent2(101, 102)
+        )
+        entity2 = ExampleEntity(
+            comp1=ExampleComponent(3, 4), comp2=ExampleComponent2(103, 104)
+        )
+
+        assert np.all(ExampleEntity.comp1.x == np.array([1, 3], float))
+        assert np.all(ExampleEntity.comp1.y == np.array([2, 4], float))
+        assert np.all(ExampleEntity.comp2.z == np.array([101, 103], float))
+        assert np.all(ExampleEntity.comp2.w == np.array([102, 104], float))
